cd14bdfadbd30ab6e7a978718994a55e6ef1e539
UK2bi <ukmainor@gmail.com>
2023-04-20 11:12:45 +0300

    Commit

---
+++ lib/screens/weather-screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart';
+import '../controllers/weather-conroller.dart';
+
+class WeatherScreen extends StatefulWidget {
+  const WeatherScreen({Key? key});
+
+  @override
+  State<WeatherScreen> createState() => _WeatherScreenState();
+}
+
+class _WeatherScreenState extends State<WeatherScreen> {
+
+  late Future<String> _weatherData;
+
+  @override
+  void initState() {
+    super.initState();
+    _weatherData = WeatherController().fetchWeather();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Weather in Tallinn'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {
+              setState(() {
+                _weatherData = WeatherController().fetchWeather();
+              });
+            },
+          ),
+        ],
+      ),
+      body: FutureBuilder(
+        future: _weatherData,
+        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          } else {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  snapshot.data ?? '',
+                  style: TextStyle(fontSize: 32),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            );
+          }
+        },
+      ),
+
+      backgroundColor: Colors.blueGrey,
+
+    );
+  }
+}
